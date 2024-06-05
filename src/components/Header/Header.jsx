eaf5927e46baf099610cfc1a95a0916fddab82e4
shafikul7 <shafikul1170@gmail.com>
2024-06-05 23:07:45 +0600

    project update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,6 @@ import {
   faPenNib,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faFacebookF } from '@fortawesome/free-solid-svg-icons';
-// import { faLinkedin } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
   return (
@@ -19,22 +17,18 @@ const Header = () => {
             </a>
 
             <a href="" className="text-white text-xs lg:text-sm">
-              {/* <i className="fa fa-envelope"></i> */}
+
               contact@mediusware.com
             </a>
           </div>
           <div className="flex justify-end  gap-3 text-primary">
-            {/* <FontAwesomeIcon icon="fa-brands fa-facebook" /> */}
-            {/* <FontAwesomeIcon icon={faFacebook} /> */}
             <FontAwesomeIcon icon={faPenNib} />
             <FontAwesomeIcon icon={faEnvelope} />
             <FontAwesomeIcon icon={faFaceSmileBeam} />
-            {/* <FontAwesomeIcon icon={["fal", "coffee"]} /> */}
             <FontAwesomeIcon icon={faCoffee} />
-            <FontAwesomeIcon icon="fab fa-react" />
 
-            {/* <FontAwesomeIcon icon={faFacebookF} /> */}
-            {/* <FontAwesomeIcon icon={faLinkedin} /> */}
+            
+           
           </div>
         </div>
       </div>
